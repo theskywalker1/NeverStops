8ba0d57d8d47faf1a8237740407e37c23310f14b
Luke <lucas.j.powers@ou.edu>
2020-10-21 16:45:37 -0500

    Modified it

--- src/SuperStack.java
+++ src/SuperStack.java
@@ -1,4 +1,36 @@
 
-public class SuperStack {
+public class SuperStack<T> {
+	
+	private T[] items;
+	private int top;
+	
+	public SuperStack<T>(int size) {
+		
+		items = (T[]) new Object[];
+		
+	}
+	
+	public boolean isEmpty() {
+	
+	    //TODO
+		return true;
+	}
+	
+	public boolean isFull() {
+		
+		//TODO
+		return true;
+	}
+	
+	public boolean push(T item) {
+		
+		//TODO
+		return true; 
+	}
+	
+	public T pop() {
+		
+		return null;
+	}
 
 }
