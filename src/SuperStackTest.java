24c0e2144d84e60f2e327208e733531f778252d2
Luke <lucas.j.powers@ou.edu>
2020-10-21 16:45:52 -0500

    Added test case. Empty

---
+++ src/SuperStackTest.java
@@ -0,0 +1,63 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author lukes
+ *
+ */
+class SuperStackTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link SuperStack#isEmpty()}.
+	 */
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SuperStack#isFull()}.
+	 */
+	@Test
+	void testIsFull() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SuperStack#push(java.lang.Object)}.
+	 */
+	@Test
+	void testPush() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SuperStack#pop()}.
+	 */
+	@Test
+	void testPop() {
+		fail("Not yet implemented");
+	}
+
+}
