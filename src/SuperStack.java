c58ed664005a6f39cd9a59d54f1b0533550ec822
Luke <lucas.j.powers@ou.edu>
2020-10-21 17:01:13 -0500

    Tested isEmpty

--- src/SuperStack.java
+++ src/SuperStack.java
@@ -12,25 +12,29 @@ public class SuperStack<T> {
 	
 	public boolean isEmpty() {
 	
-	    //TODO
-		return true;
+		return top == 0;
 	}
 	
 	public boolean isFull() {
 		
-		//TODO
-		return true;
+		return top == items.length;
 	}
 	
 	public boolean push(T item) {
 		
-		//TODO
+		if (isFull())
+			return false;
+		items[top] = item;
 		return true; 
 	}
 	
 	public T pop() {
-		
-		return null;
+		if(isEmpty()) 
+			return null;
+		else {
+			top -=1;
+			return items[top];
+		}
 	}
 
 }
